c33f734c20ff4703ab8427a1f25923f95c57bf9a
n-nishimura <n.nishimurara@gmail.com>
2022-01-17 22:07:29 +0900

    face

--- pytello/tools/image_face_detect.py
+++ pytello/tools/image_face_detect.py
@@ -8,5 +8,13 @@ img = cv.imread('photo-py.jpeg')
 
 # grayイメージを作る
 gray = cv.cvtColor(img,cv.COLOR_BG2GRAY)
+faces = face_cascade.detectMultiScale(gray , 1.3 , 5)
 
-4;00
+print(len(faces))
+# faceとして認識されたものがいくつあるか
+
+# 顔を四角で囲む
+for (x , y , w , h) in faces:
+  cv.rectangle (img ,(x,y) , (x+w, y+h),(255,0,0),2)
+
+
