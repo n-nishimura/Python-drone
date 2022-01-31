c961fd0510efffb77657c94e30e5ade3d00d2fd2
n-nishimura <n.nishimurara@gmail.com>
2022-01-31 17:01:34 +0900

    self 618

---
+++ pytello/drine_manager.py
@@ -0,0 +1,21 @@
+import logging
+import socket
+import sys
+import time
+
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
+
+class DroneManager(object):
+  def __init__(self, host_ip='192.168.10.2', host_port=8889,
+              drone_ip='192.168.10.1', drone_port=8889):
+      self.host_ip = host_ip
+      self.host_port = host_port
+      self.drone_ip = drone_ip
+      self.drone_port = drone_port
+      self.drone_address = (drone_ip, drone_port)
+      self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+
